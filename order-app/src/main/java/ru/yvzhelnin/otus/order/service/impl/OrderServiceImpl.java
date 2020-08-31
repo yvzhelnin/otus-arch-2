5b7e19f6dcccf1598efb7162b06cfb61efa5dd96
Yury.Zhelnin <yury.zhelnin@x5.ru>
2020-08-31 19:37:56 +0300

    shop - fixed withdrawal request

--- order-app/src/main/java/ru/yvzhelnin/otus/order/service/impl/OrderServiceImpl.java
+++ order-app/src/main/java/ru/yvzhelnin/otus/order/service/impl/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.yvzhelnin.otus.order.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -18,6 +20,7 @@ import java.util.Map;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     public static final String CLIENT_ID_HEADER = "X-Client-Id";
 
@@ -33,7 +36,7 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public void placeOrder(String clientId, BigDecimal cost) {
         final String url = billingAppUrl + "/" + clientId;
-
+        LOGGER.info("Assembled URL for money withdrawing: '{}'", url);
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
@@ -41,8 +44,9 @@ public class OrderServiceImpl implements OrderService {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("sum", cost.toString());
         HttpEntity requestEntity = new HttpEntity(headers);
-        ResponseEntity response = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class, parameters);
+        ResponseEntity response = restTemplate.exchange(url, HttpMethod.PATCH, requestEntity, String.class, parameters);
 
+        LOGGER.info("Status: {}, sending the notification", response.getStatusCode());
         if (response.getStatusCode() == HttpStatus.OK) {
             notificationService.sendNotification(NotificationType.SUCCESS, clientId, cost);
         } else {
