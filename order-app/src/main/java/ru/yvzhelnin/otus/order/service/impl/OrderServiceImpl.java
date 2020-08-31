1b8b7ebc7fe65741f4528bf8b84259aca8dcda5d
Yury.Zhelnin <yury.zhelnin@x5.ru>
2020-08-31 20:06:54 +0300

    shop - made app able to perform patch requests

--- order-app/src/main/java/ru/yvzhelnin/otus/order/service/impl/OrderServiceImpl.java
+++ order-app/src/main/java/ru/yvzhelnin/otus/order/service/impl/OrderServiceImpl.java
@@ -3,13 +3,11 @@ package ru.yvzhelnin.otus.order.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.yvzhelnin.otus.order.dto.notification.NotificationType;
@@ -29,12 +27,9 @@ public class OrderServiceImpl implements OrderService {
     @Value("${order.billing-app.url.withdraw}")
     private String billingAppUrl;
 
-    private final RestTemplateBuilder restTemplateBuilder;
-
     private final NotificationService notificationService;
 
-    public OrderServiceImpl(RestTemplateBuilder restTemplateBuilder, NotificationService notificationService) {
-        this.restTemplateBuilder = restTemplateBuilder;
+    public OrderServiceImpl(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
 
@@ -42,14 +37,14 @@ public class OrderServiceImpl implements OrderService {
     public void placeOrder(String clientId, BigDecimal cost) {
         final String url = billingAppUrl + "/" + clientId;
         LOGGER.info("Assembled URL for money withdrawing: '{}'", url);
-        RestTemplate restTemplate = restTemplateBuilder.requestFactory(HttpComponentsClientHttpRequestFactory.class).build();
+        RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
         headers.set(CLIENT_ID_HEADER, clientId);
         Map<String, String> parameters = new HashMap<>();
         parameters.put("sum", cost.toString());
         HttpEntity requestEntity = new HttpEntity(headers);
-        ResponseEntity response = restTemplate.exchange(url, HttpMethod.PATCH, requestEntity, String.class, parameters);
+        ResponseEntity response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class, parameters);
 
         LOGGER.info("Status: {}, sending the notification", response.getStatusCode());
         if (response.getStatusCode() == HttpStatus.OK) {
